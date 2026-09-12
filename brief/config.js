2fcf19c03a7d4f8d0c4883d59daf2d566d589dad
ChipButt <jameschipbutt@hotmail.com>
2026-09-12 20:17:59 +0100

    Remove obsolete FormSubmit intake config

--- brief/config.js
+++
@@ -1,4 +0,0 @@
-// Website brief delivery configuration.
-// Create/activate a FormSubmit endpoint, then paste it below.
-// Recommended format: https://formsubmit.co/YOUR-RANDOM-STRING
-window.HOSPO_BRIEF_ENDPOINT = "";
